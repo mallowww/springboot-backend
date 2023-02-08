8f6926605f5f3fe2ec1ee4d005ecbe1dce5204ff
mell <chanon4817@gmail.com>
2023-02-08 23:20:54 +0700

    Init

---
+++ src/test/java/com/springb/learning/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springb.learning.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
