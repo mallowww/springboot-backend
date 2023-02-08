90eb34a400102346696cf032f144f1557c11f566
mell <chanon4817@gmail.com>
2023-02-09 00:09:42 +0700

    feat: hello

---
+++ src/main/java/com/springb/learning/backend/api/TestApi.java
@@ -0,0 +1,13 @@
+package com.springb.learning.backend.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/test")
+public class TestApi {
+    @GetMapping
+    public String test() {
+        return "Hello Test";
+    }
+}
---
+++ src/main/java/com/springb/learning/backend/model/TestResponse.java
@@ -0,0 +1,9 @@
+package com.springb.learning.backend.model;
+
+import lombok.Data;
+
+@Data
+public class TestResponse {
+    private String name;
+    private String food;
+}
