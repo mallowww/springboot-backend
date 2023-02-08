b8e7767f1426025fb1001179f67987aadf8a415c
mell <chanon4817@gmail.com>
2023-02-09 00:24:48 +0700

    feat: sample of id path mapping

--- src/main/java/com/springb/learning/backend/api/TestApi.java
+++ src/main/java/com/springb/learning/backend/api/TestApi.java
@@ -1,13 +1,32 @@
 package com.springb.learning.backend.api;
 
+import com.springb.learning.backend.model.TestResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestApi {
     @GetMapping
-    public String test() {
-        return "Hello Test";
+    public TestResponse test() {
+        TestResponse response = new TestResponse();
+        response.setName("Olly");
+        response.setFood("Pizza");
+        return  response;
+    }
+
+    @GetMapping
+    @RequestMapping("/2")
+    public TestResponse test2() {
+        TestResponse response = new TestResponse();
+        response.setName("Olly 2");
+        response.setFood("Pizza 2");
+        return  response;
+    }
+
+    @PostMapping
+    private String register() {
+        return "";
     }
 }
